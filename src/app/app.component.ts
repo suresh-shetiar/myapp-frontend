d57adb80a8f97f468dcd37ad6031a03aae938776
Rahul M <rahul@fourzip.com>
2018-11-03 19:34:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  searchStr = null;
+  vehicleObj = {};
+  keysArray = [];
+  httpResponse = false;
+  invalidInput = false;
+  constructor(private http:HttpClient)
+  {
+    console.log('searchStr : ', this.searchStr);
+  }
+  search() {
+    // console.log('search : ', this.searchStr);
+    // this.findByStr();
+    if(this.searchStr.length === 0)
+    {
+      this.invalidInput = true;
+    } else {
+      this.invalidInput = false;
+    }
+  }
+
+  findByStr()
+  {
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Content-Type':  'application/json'
+    //   })
+    // };
+    if(this.searchStr.length > 0)
+    {
+      this.invalidInput = false;
+      this.httpResponse = true;
+      this.http.get('http://13.127.167.171:3000/api/data?search=' + this.searchStr).subscribe((response) => {
+       console.log('response : ', response);
+       this.vehicleObj = response;
+       this.keysArray = Object.keys(response);
+      //  console.log('response : ', this.keysArray);
+       this.httpResponse = false;
+   }, (error) => {
+     this.httpResponse = false;
+    //  console.log('error : ', error);
+   });
+    } else {
+      this.invalidInput = true;
+    }
+
+
+  }
 }
